bbc6fb5d07894dd1c88868948c500538871f6970
Mohammed, Nawaz <nawaz.mohammed@tyson.com>
2020-09-15 03:22:45 +0530

    Updates

---
+++ precache-manifest.91563383254c0d461e9394d8917230c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba014549a78635d28b8b9e192b76d126",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "c5086a5eeff21cd48086",
+    "url": "/movie-search/static/css/2.86b33428.chunk.css"
+  },
+  {
+    "revision": "7a48495551615d3b735e",
+    "url": "/movie-search/static/css/main.b10131f0.chunk.css"
+  },
+  {
+    "revision": "c5086a5eeff21cd48086",
+    "url": "/movie-search/static/js/2.e76da6de.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/movie-search/static/js/2.e76da6de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a48495551615d3b735e",
+    "url": "/movie-search/static/js/main.d562a1c7.chunk.js"
+  },
+  {
+    "revision": "823c417190fe8f5322d4",
+    "url": "/movie-search/static/js/runtime-main.a21bebd1.js"
+  },
+  {
+    "revision": "70e45f4403a2ffacf06c7a66910bd527",
+    "url": "/movie-search/static/media/coming_soon.70e45f44.gif"
+  },
+  {
+    "revision": "0775a7d601bdba842e345bf231a565c7",
+    "url": "/movie-search/static/media/no_data.0775a7d6.jpg"
+  }
+]);
